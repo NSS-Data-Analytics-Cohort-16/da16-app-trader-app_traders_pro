6312dccc7680a1e6858050b6f459c5cd0d9dd67e
BC1130 <bcausey48@gmail.com>
2025-10-18 13:06:43 -0500

    brenda3

--- data/app_traders_pro_brenda.sql
+++ data/app_traders_pro_brenda.sql
@@ -21,6 +21,50 @@ USING(name)
 GROUP BY app_store.name, play_store.rating
 ORDER BY rating DESC
 
+SELECT
+    name,
+    REPLACE(price, '$', '')::numeric AS price,
+    'playstore' AS store,
+    CASE 
+        WHEN REPLACE(price, '$', '')::numeric <= 1.00 THEN 10000.0
+        ELSE REPLACE(price, '$', '')::numeric * 10000.0
+    END AS purchase_price,
+    CASE 
+        WHEN name IN (
+            SELECT name FROM app_store_apps
+            INTERSECT
+            SELECT name FROM play_store_apps
+        ) THEN 2
+        ELSE 1
+    END AS store_count,
+    1000 AS per_month
+
+FROM play_store_apps
+
+UNION
+
+SELECT
+    name,
+    price,
+    'appstore' AS store,
+    CASE
+        WHEN price <= 1.00 THEN 10000.0
+        ELSE price * 10000.0
+    END AS purchase_price,
+    CASE 
+        WHEN name IN (
+            SELECT name FROM app_store_apps
+            INTERSECT
+            SELECT name FROM play_store_apps
+        ) THEN 2
+        ELSE 1
+    END AS store_count,
+    1000 AS per_month
+
+FROM app_store_apps
+
+ORDER BY price DESC, purchase_price DESC
+
 
 b. Apps earn $5000 per month, per app store it is on, from in-app advertising and in-app purchases, regardless of the price of the app.
     
@@ -78,7 +122,7 @@ SELECT
 	a.name,
 	p.rating,
 	a.rating,
-	ROUND(AVG(p.rating + a.rating), 0) AS avg_rating
+	ROUND((p.rating + a.rating) /2, 2) AS avg_rating
 FROM play_store_apps a
 FULL OUTER JOIN app_store_apps p
 USING (name)
@@ -89,13 +133,6 @@ ORDER BY avg_rating DESC;
 
 
 e. App Trader would prefer to work with apps that are available in both the App Store and the Play Store since they can market both for the same $1000 per month.
-
-SELECT name
-FROM app_store_apps
-LEFT JOIN play_store_apps
-USING(name)
-GROUP BY name
-ORDER BY name ASC
 
 SELECT name
 FROM app_store_apps
@@ -176,3 +213,134 @@ FROM (
     FROM app_store_apps a
     INNER JOIN play_store_apps p ON a.name = p.name
 ) AS joined_apps;
+
+
+
+SELECT
+	name,
+	ROUND(ROUND(AVG(psa.rating) * 2) / 2,2) AS rounded_rating,
+CASE
+	WHEN ROUND(AVG(psa.rating) * 2) / 2 = 0.0 THEN 1
+	WHEN ROUND(AVG(psa.rating) * 2) / 2 = 0.5 THEN 2
+	WHEN ROUND(AVG(psa.rating) * 2) / 2 = 1.0 THEN 3
+	WHEN ROUND(AVG(psa.rating) * 2) / 2 = 1.5 THEN 4
+	WHEN ROUND(AVG(psa.rating) * 2) / 2 = 2.0 THEN 5
+	WHEN ROUND(AVG(psa.rating) * 2) / 2 = 2.5 THEN 6
+	WHEN ROUND(AVG(psa.rating) * 2) / 2 = 3.0 THEN 7
+	WHEN ROUND(AVG(psa.rating) * 2) / 2 = 3.5 THEN 8
+	WHEN ROUND(AVG(psa.rating) * 2) / 2 = 4.0 THEN 9
+	WHEN ROUND(AVG(psa.rating) * 2) / 2 = 4.5 THEN 10
+	WHEN ROUND(AVG(psa.rating) * 2) / 2 = 5.0 THEN 11
+
+(SELECT
+	name,
+	REPLACE(price, '$', '')::numeric AS price,
+	'playstore' AS store,
+	CASE WHEN
+		REPLACE(price, '$', '')::numeric <= 1.00::numeric then 10000.0
+		WHEN
+		REPLACE(price, '$', '')::numeric <= 1.00::numeric then REPLACE(price, '$', '')::numeric * 10000.0
+		ELSE '0'
+	END AS purchase_price
+FROM play_store_apps)
+UNION
+(SELECT
+	name,
+	price,
+	'appstore' AS store,
+	CASE
+		WHEN price <= 1.00 then 10000.0
+		WHEN price > 1.00 then price * 10000.0
+		ELSE '0'
+		END AS purchase_price,
+		CASE WHEN
+		name IN (
+			SELECT
+				name
+			FROM app_store_apps)
+			INTERSECT
+			(SELECT
+			name
+		FROM play_store_apps
+		) then 2
+		ELSE 1
+		END AS store_count,
+		1000 AS per_month
+FROM app_store_apps
+)
+ORDER BY price DESC, purchase_price DESC
+			
+SELECT
+	a.name,
+	
+	case when a.rating is not null and p.rating is not null 
+	     then 10000
+		 when a.rating is not null and p.rating is not null
+		 then 5000  else 0  
+		 end *12   as monthly_earning,
+		        1000 as marketing_cost,
+			
+	ROUND((a.rating + p.rating /2) /0.5,0) *0.5 as avg_rating,
+	(ROUND(((a.rating + p.rating)/2) /0.5, 0)*0.5*2) + 1 as lifespan,
+	(1000 * 12 * ((ROUND(((p.rating + a.rating) / 2) / 0.5, 0) * 0.5 * 2) + 1)) AS lifespan_year
+	FROM app_store_apps as a
+	left join play_store_apps as p
+	on a.name = p.name
+GROUP BY a.name, a.rating, p.rating 
+
+with appstore as (
+    select
+        ap.name,
+        p.rating as p_rating,
+        a.rating as a_rating,
+        cast(replace(p.price, '$', '') as numeric) as p_price,
+        a.price as a_price,
+        p.content_rating,
+        a.primary_genre,
+        p.genres as secondary_genre
+    from (
+        select name
+        from app_store_apps
+        intersect
+        select name
+        from play_store_apps ) AS ap
+    LEFT JOIN play_store_apps AS p ON p.name = ap.name
+    LEFT JOIN app_store_apps AS a ON a.name = ap.name)
+	
+select
+    name,
+    ROUND(((p_rating + a_rating) / 2) / 0.5, 0) * 0.5 AS avg_rating,
+    (ROUND(((p_rating + a_rating) / 2) / 0.5, 0) * 0.5 * 2) + 1 AS lifespan,
+    greatest(p_price, a_price) AS max_price,
+    case
+        when greatest(p_price, a_price) <= 1 then 10000
+        else greatest(p_price, a_price) * 10000
+    end as purchase_price,
+    case
+        when p_rating is not null and a_rating is not null then 10000
+        when p_rating is not null or a_rating is not null then 5000
+        else 0
+    end as monthly_earning,
+    1000 AS marketing_cost,
+    (
+        ((ROUND(((p_rating + a_rating) / 2) / 0.5, 0) * 0.5 * 2) + 1) *
+        case
+            when p_rating is not null and a_rating is not null then 10000
+            when p_rating is not null or a_rating is not null then 5000
+            ELSE 0
+        END * 12
+    ) -
+    case
+        when greatest(p_price, a_price) <= 1 then 10000
+        else greatest(p_price, a_price) * 10000
+    end -
+    (1000 * 12 * ((ROUND(((p_rating + a_rating) / 2) / 0.5, 0) * 0.5 * 2) + 1)) AS lifespan_profit,
+    content_rating,
+    primary_genre,
+    secondary_genre,
+    'both' AS store
+from appstore
+group by name, p_rating, a_rating, p_price, a_price, content_rating, primary_genre, secondary_genre
+having ROUND((p_rating + a_rating) / 2, 1) >= 3.0
+order by lifespan_profit DESC
+limit 10;
